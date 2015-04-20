f7fb6620df40684555f17cc3cdda82f200dfe7c2
Nathan Bell <nath.bell@gmail.com>
2015-04-20 13:34:00 +0100

    Basic GUI
    Very very very basic.

---
+++ degreeCalculator/degreeCalculator/buildCourseForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace degreeCalculator
+{
+    partial class buildCourseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.secondNameLabel = new System.Windows.Forms.Label();
+            this.courseNameLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::degreeCalculator.Properties.Resources.banner_logo_university_of_westminster;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(478, 232);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(168, 307);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(191, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(168, 262);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(168, 284);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(105, 262);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(57, 13);
+            this.firstNameLabel.TabIndex = 4;
+            this.firstNameLabel.Text = "First Name";
+            this.firstNameLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // secondNameLabel
+            // 
+            this.secondNameLabel.AutoSize = true;
+            this.secondNameLabel.Location = new System.Drawing.Point(87, 284);
+            this.secondNameLabel.Name = "secondNameLabel";
+            this.secondNameLabel.Size = new System.Drawing.Size(75, 13);
+            this.secondNameLabel.TabIndex = 5;
+            this.secondNameLabel.Text = "Second Name";
+            // 
+            // courseNameLabel
+            // 
+            this.courseNameLabel.AutoSize = true;
+            this.courseNameLabel.Location = new System.Drawing.Point(91, 310);
+            this.courseNameLabel.Name = "courseNameLabel";
+            this.courseNameLabel.Size = new System.Drawing.Size(71, 13);
+            this.courseNameLabel.TabIndex = 6;
+            this.courseNameLabel.Text = "Course Name";
+            // 
+            // buildCourseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 376);
+            this.Controls.Add(this.courseNameLabel);
+            this.Controls.Add(this.secondNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "buildCourseForm";
+            this.Text = "Build Your Course";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.Label secondNameLabel;
+        private System.Windows.Forms.Label courseNameLabel;
+    }
+}
---
+++ degreeCalculator/degreeCalculator/mainForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace degreeCalculator
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabOne = new System.Windows.Forms.TabPage();
+            this.tabTwo = new System.Windows.Forms.TabPage();
+            this.tabThree = new System.Windows.Forms.TabPage();
+            this.tabFour = new System.Windows.Forms.TabPage();
+            this.tabFive = new System.Windows.Forms.TabPage();
+            this.buildCourseButton = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabOne.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabOne);
+            this.tabControl1.Controls.Add(this.tabTwo);
+            this.tabControl1.Controls.Add(this.tabThree);
+            this.tabControl1.Controls.Add(this.tabFour);
+            this.tabControl1.Controls.Add(this.tabFive);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(472, 391);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabOne
+            // 
+            this.tabOne.Controls.Add(this.buildCourseButton);
+            this.tabOne.Location = new System.Drawing.Point(4, 22);
+            this.tabOne.Name = "tabOne";
+            this.tabOne.Padding = new System.Windows.Forms.Padding(3);
+            this.tabOne.Size = new System.Drawing.Size(464, 365);
+            this.tabOne.TabIndex = 0;
+            this.tabOne.Text = "Home";
+            this.tabOne.UseVisualStyleBackColor = true;
+            // 
+            // tabTwo
+            // 
+            this.tabTwo.Location = new System.Drawing.Point(4, 22);
+            this.tabTwo.Name = "tabTwo";
+            this.tabTwo.Padding = new System.Windows.Forms.Padding(3);
+            this.tabTwo.Size = new System.Drawing.Size(464, 365);
+            this.tabTwo.TabIndex = 1;
+            this.tabTwo.Text = "Level Four";
+            this.tabTwo.UseVisualStyleBackColor = true;
+            // 
+            // tabThree
+            // 
+            this.tabThree.Location = new System.Drawing.Point(4, 22);
+            this.tabThree.Name = "tabThree";
+            this.tabThree.Size = new System.Drawing.Size(464, 365);
+            this.tabThree.TabIndex = 2;
+            this.tabThree.Text = "Level Five";
+            this.tabThree.UseVisualStyleBackColor = true;
+            // 
+            // tabFour
+            // 
+            this.tabFour.Location = new System.Drawing.Point(4, 22);
+            this.tabFour.Name = "tabFour";
+            this.tabFour.Size = new System.Drawing.Size(464, 365);
+            this.tabFour.TabIndex = 3;
+            this.tabFour.Text = "Level Six";
+            this.tabFour.UseVisualStyleBackColor = true;
+            // 
+            // tabFive
+            // 
+            this.tabFive.Location = new System.Drawing.Point(4, 22);
+            this.tabFive.Name = "tabFive";
+            this.tabFive.Size = new System.Drawing.Size(464, 365);
+            this.tabFive.TabIndex = 4;
+            this.tabFive.Text = "Summary";
+            this.tabFive.UseVisualStyleBackColor = true;
+            // 
+            // buildCourseButton
+            // 
+            this.buildCourseButton.Location = new System.Drawing.Point(19, 17);
+            this.buildCourseButton.Name = "buildCourseButton";
+            this.buildCourseButton.Size = new System.Drawing.Size(75, 23);
+            this.buildCourseButton.TabIndex = 0;
+            this.buildCourseButton.Text = "Build Course";
+            this.buildCourseButton.UseVisualStyleBackColor = true;
+            this.buildCourseButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 415);
+            this.Controls.Add(this.tabControl1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Degree Calculator";
+            this.tabControl1.ResumeLayout(false);
+            this.tabOne.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabOne;
+        private System.Windows.Forms.Button buildCourseButton;
+        private System.Windows.Forms.TabPage tabTwo;
+        private System.Windows.Forms.TabPage tabThree;
+        private System.Windows.Forms.TabPage tabFour;
+        private System.Windows.Forms.TabPage tabFive;
+    }
+}
+
